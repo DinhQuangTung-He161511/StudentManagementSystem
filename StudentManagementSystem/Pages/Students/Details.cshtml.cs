7e8b8d34ae38ff0e0b10f92a2a4cbbaba82513b6
DinhQuangTung-He161511 <tungdqhe161511@fpt.edu.vn>
2024-03-22 11:13:29 +0700

    Add project files.

---
+++ StudentManagementSystem/Pages/Students/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StudentManagementSystem.Models;
+
+namespace StudentManagementSystem.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly StudentManagementSystem.Models.PRN221_StudentManagementSystemContext _context;
+
+        public DetailsModel(StudentManagementSystem.Models.PRN221_StudentManagementSystemContext context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null || _context.Students == null)
+            {
+                return NotFound();
+            }
+            ViewData["MajorId"] = new SelectList(_context.Majors, "MajorId", "MajorName");
+            var student = await _context.Students.FirstOrDefaultAsync(m => m.StudentId == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Student = student;
+            }
+            return Page();
+        }
+    }
+}
