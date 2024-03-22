fc26a65945041afe50e7be6ec1bfc879e682f19f
DinhQuangTung-He161511 <tungdqhe161511@fpt.edu.vn>
2024-03-22 13:38:39 +0700

    tung

--- StudentManagementSystem/Pages/Students/Create.cshtml.cs
+++ StudentManagementSystem/Pages/Students/Create.cshtml.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -11,29 +8,26 @@ namespace StudentManagementSystem.Pages.Students
 {
     public class CreateModel : PageModel
     {
-        private readonly StudentManagementSystem.Models.PRN221_StudentManagementSystemContext _context;
+        private readonly PRN221_StudentManagementSystemContext _context;
 
-        public CreateModel(StudentManagementSystem.Models.PRN221_StudentManagementSystemContext context)
+        public CreateModel(PRN221_StudentManagementSystemContext context)
         {
             _context = context;
         }
 
         public IActionResult OnGet()
         {
-            ViewData["MajorId"] = new SelectList(_context.Majors, "MajorId", "MajorId");
             ViewData["ClassId"] = new SelectList(_context.Classes, "ClassId", "ClassClassName");
-
+            ViewData["MajorId"] = new SelectList(_context.Majors, "MajorId", "MajorName");
             return Page();
         }
 
         [BindProperty]
-        public Student Student { get; set; } = default!;
+        public Student Student { get; set; }
 
-
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid || _context.Students == null || Student == null)
+            if (!ModelState.IsValid)
             {
                 return Page();
             }
